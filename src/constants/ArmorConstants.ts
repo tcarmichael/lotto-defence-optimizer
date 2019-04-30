c24cf9d70233875bfcdefd53e3eedb541ef59aa0
Tandy Carmichael <tcarmichael@frontier.com>
2019-04-29 22:24:17 -0500

    Add prototype of armor values

--- src/constants/ArmorConstants.ts
+++ src/constants/ArmorConstants.ts
@@ -7,7 +7,15 @@ export function getArmor(round: number, difficulty: Difficulty): number {
 }
 
 function getArmorInternal(round: number): number {
-    return 0;
+    if (round < 160) {
+        return 0;
+    } else if (round < 180) {
+        return (round - 160 + 1) * 4;
+    } else if (round < 200) {
+        return (180 - 160) * 4 + (round - 180 + 1) * 8;
+    } else if (round < 220) {
+        return (180 - 160) * 4 + (200 - 180) * 8 + (round - 200 + 1) * 12;
+    }
 }
 
 function getArmorTocInternal(round: number): number {
